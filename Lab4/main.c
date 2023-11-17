9a9d2f24653175df3eebebe1f00a1d2b0db4fc36
jekasrs <jekasjeny1012@gmail.com>
2023-11-17 09:23:38 +0100

    clean code

--- Lab4/main.c
+++ Lab4/main.c
@@ -1,29 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Location/point.h"
+#include "functions.h"
 
-// task2
-
-void theLongestLength(double locations[][2], int M, double* maxDist, int* maxPoint1, int* maxPoint2);
+void task1();
+void task2();
+void theLongestLength(double[][2], int, double* , int* , int* );
 
 int main() {
+    task1();
+    task2();
+    return 0;
+}
+
+void theLongestLength(double locations[][2], int M, double* maxDist, int* maxPoint1, int* maxPoint2) {
+    *maxDist = 0.0;
+    *maxPoint1 = 0;
+    *maxPoint2 = 0;
+
+    for (int i = 0; i < M; i++) {
+        double cur_lat1 = locations[i][0];
+        double cur_lon1 = locations[i][1];
+        for (int j = 0; j < M; j++) {
+            if (i == j) continue;
+            double lat1 = locations[j][0];
+            double lon1 = locations[j][1];
+
+            double current_distance = distanceKm(cur_lat1, cur_lon1, lat1, lon1);
+            if (*maxDist < current_distance) {
+                *maxDist = current_distance;
+                *maxPoint1 = i;
+                *maxPoint2 = j;
+            }
+        }
+    }
+}
+
+void task1() {
     int N = 9;
     double **points = (double **) malloc(N * sizeof(double *));
     for (int i = 0; i < N; ++i) {
         points[i] = (double *) malloc(2 * sizeof(double));
     }
 
-    // reading data from .txt
-    FILE *file = fopen("/Users/evgenijsmirnov/HAW_HAMBURG/SOL1/Lab4/Geo.txt", "r");
+    FILE *file = fopen("Geo.txt", "r");
     if (file == NULL) {
         fprintf(stderr, "Can't open file.\n");
-        return 1;
+        return;
     }
     while (!feof(file)) {
         for (int j = 0; j < N; j++) {
             fscanf(file, "%lf,%lf\n", &points[j][0], &points[j][1]);
         }
     }
+    fclose(file);
 
     double distance = 0.0;
     for (int j = 0; j < N - 1; j++) {
@@ -34,7 +63,13 @@ int main() {
 
     printf("Distance is: %.3f km\n", distance);
 
-    // Task2
+    for (int i = 0; i < N; ++i) {
+        free(points[i]);
+    }
+    free(points);
+
+}
+void task2() {
     char *names[] = {"HAW Hamburg",
                      "Eiffel Tower",
                      "Palma de Mallorca",
@@ -61,29 +96,4 @@ int main() {
 
     printf("%s and %s have the largest distance (%.2f km).\n"
            "You’d better not walk. Take a flight , instead.\n", names[point1], names[point2], maxDist);
-
-    return 0;
-}
-
-void theLongestLength(double locations[][2], int M, double* maxDist, int* maxPoint1, int* maxPoint2) {
-    *maxDist = 0.0;
-    *maxPoint1 = 0;
-    *maxPoint2 = 0;
-
-    for (int i = 0; i < M; i++) {
-        double cur_lat1 = locations[i][0];
-        double cur_lon1 = locations[i][1];
-        for (int j = 0; j < M; j++) {
-            if (i == j) continue;
-            double lat1 = locations[j][0];
-            double lon1 = locations[j][1];
-
-            double current_distance = distanceKm(cur_lat1, cur_lon1, lat1, lon1);
-            if (*maxDist < current_distance) {
-                *maxDist = current_distance;
-                *maxPoint1 = i;
-                *maxPoint2 = j;
-            }
-        }
-    }
 }
--- library.h
+++
@@ -1,6 +0,0 @@
-#ifndef LABS_LIBRARY_H
-#define LABS_LIBRARY_H
-
-void hello(void);
-
-#endif //LABS_LIBRARY_H
